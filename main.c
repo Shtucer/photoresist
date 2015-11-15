c1fabf25283c078ca7cf3c50e2f49eabd7b91342
Alex Kroll <krollus@gmail.com>
2015-11-16 01:50:17 +0300

    Decomposition files

--- main.c
+++ main.c
@@ -1,69 +1,56 @@
 /*
  * main.c
  *
- *  Created on: 27 окт. 2015 г.
+ *  Created on: 27 пїЅпїЅпїЅ. 2015 пїЅ.
  *      Author: Shtucer
  */
-#define F_CPU	12000000UL
+//#define F_CPU	12000000UL
 
 #include <avr/io.h>
 
 #include <util/delay.h>
 #include <avr/interrupt.h>
 
-
-// Buttons
-#define	SW1 PB2
-#define SW2	PB3
-#define SW3	PB4
-#define SW4	PB5
-
-#define IsPressed(x) ((PINB) & _BV(x))
-
-#define RELAY PB0
-
-#define RelayOn() (PINB |= (1 << RELAY))
-#define RelayOff() (PINB &= ~(1 << RELAY))
+#include "seg_defines.h"
+#include "config.h"
 
 
-
-void display(int);
-// dp G F D E C B A
-unsigned char DIGITS[]={
-		0b11000000	// 0
-		,0b11111001	// 1
-		,0b10100100	// 2
-		,0b10101000	// 3
-		,0b10011001	// 4
-		,0b10001010	// 5
-		,0b10000010	// 6
-		,0b11111000	// 7
-		,0b10000000	// 8
-		,0b10001000	// 9
-};
-
-unsigned char SYMBOLS[]={
-		 0b10111111 // -
-
-};
 // Video memory
 unsigned char data[4] = {0,0,0,0};
+unsigned int exposeTime = 0;
 
-volatile unsigned char count=0;
+#define relayIsOn() (PINB & PB0)
 
-int clkcounter=0;
-volatile int i = 1234;
+
+
+
+#define IDLE			0
+#define EXPOSE			1
+#define SETUP			3
+
+
+/*
+#define MINUTES_SETUP 	2
+#define SECONDS_SETUP 	3
+*/
+
+
+
+volatile int	STATUS = IDLE;
 
 // Update display
 ISR(TIMER0_OVF_vect){
+	static unsigned char count=0;
 #ifdef DEBUG
 	static char totalOVF=0;
 	if(totalOVF>=0){
 #endif
+
+	PORTC ^= _BV(count);
+	count = (count+1)%4;
+	PORTD = data[count];
 	PORTC = ~_BV(count);
 
-	PORTD = data[count];
-	count = (count+1)%4;
 #ifdef DEBUG
 	totalOVF=0;
 	}
@@ -72,19 +59,28 @@ ISR(TIMER0_OVF_vect){
 
 }
 
-
+// Expose timer
+ISR(TIMER1_COMPA_vect){
+	if((STATUS & EXPOSE) && (exposeTime > 1))
+	{
+		exposeTime -= 1;
+	}
+	else
+	{
+		exposeTime = 0;
+		STATUS = IDLE;
+	}
+}
 
 // Check the Buttons
 ISR(TIMER2_OVF_vect){
-
 	if(IsPressed(SW1)){
 		PORTC |= _BV(PC4);
+
 	}
 	else {
 		PORTC &= ~_BV(PC4);
 	}
-
-	//PORTC ^= _BV(PC4);
 }
 
 
@@ -110,7 +106,7 @@ void setup_io(){
 
 	DDRB |= (1 << RELAY);
 	PORTB &= ~(1 << RELAY);
-	RelayOn();
+
 
 	DDRB |= 0 << SW1;
 	DDRB |= 0 << SW2;
@@ -137,6 +133,19 @@ void setup_io(){
 	PORTD = 0xff; //0b11111111; // OFF all segments
 
 
+}
+
+void startButtonTimer(){
+	// Timer2 - button events
+	// every 1ms
+	// /8 prescaler
+	ASSR |= (0 << AS2); // Connect timer to TOSC1
+	TCCR2 |=  (0 << CS22) | (1 << CS21) | (1 << CS20);
+	TCNT2 = 47;
+	TIMSK |= (1 << TOIE2);
+}
+
+void startDisplayUpdateTimer(){
 #ifdef DEBUG
 	TCNT0 = 220;
 #else
@@ -147,21 +156,24 @@ void setup_io(){
 	// /8 prescaler
 	TCCR0 |=  (0 << CS02) | (1 << CS01) | (0 << CS00);
 	// start timer0
-	//TIMSK |= (1 << TOIE0);
+	TIMSK |= (1 << TOIE0);
 
+}
+
+void stopExpositionTimer() {
+	TIMSK &= ~(1 << OCIE1A);
+}
+
+void startExpositionTimer() {
+	// Start timer 0x2DC6
 	// Timer1 - time counter (every 1sec)
+	// Prescaler 1024
+	TCCR1B |= (1 << CS02) | (0 << CS01) | (1 << CS00);
+	TCCR1B |= (1 << WGM12); // configure timer1 for CTC mode
+	//OCR1A = 0x2dc6;
+	OCR1A = (F_CPU/(2*1024))-1;
 
-
-//	TCCR1B |= (0 << CS02) | (1 << CS01) | (1 << CS00);
-//	TCNT1 = 0x02;
-
-	// Timer2 - button events
-	// every 1ms
-	// /8 prescaler
-	ASSR |= (0 << AS2); // Connect timer to TOSC1
-	TCCR2 |=  (0 << CS22) | (1 << CS21) | (1 << CS20);
-	TCNT2 = 47;
-	TIMSK |= (1 << TOIE2);
+	TIMSK |= (1 << OCIE1A);
 }
 /*
 void _display_digit(int pos, int dig){
@@ -187,27 +199,40 @@ void display(int integer){
 }
 */
 
+void display(int i){
+	data[0] = DIGITS[i % 10];
+	data[1] = DIGITS[(i/10)%10];
+	data[2] = DIGITS[(i/100)%10];
+	data[3] = DIGITS[(i/1000)];
+}
+
+
 int main(void){
 //	cli();
 	setup_io();
 
+	STATUS = IDLE;
+	startDisplayUpdateTimer();
+	startButtonTimer();
+
 	sei();
 
-
-	//short pos = 0;
 	while(1){
-		data[0] = DIGITS[i % 10];
-		data[1] = DIGITS[(i/10)%10];
-		data[2] = DIGITS[(i/100)%10];
-		data[3] = DIGITS[(i/1000)];
-		//_display_digit(count, data[count]);
+		switch(STATUS){
+		case IDLE:
+			break;
+		case EXPOSE:
+			if(relayIsOn()) {
 
-//		display(0);
-//		PORTC |= ((1<<PC0) | (1<<PC1) | (1<<PC2) | (1<<PC3));
-//		PORTC &= ~(1 << POS[0]);
-//		_delay_ms(10);
-		//i++;
-		//PORTB ^= _BV(PB5);
+			}
+			break;
+		case SETUP:
+			break;
+		default:
+			break;
+		}
+
+
 	}
 	return 0;
 }
